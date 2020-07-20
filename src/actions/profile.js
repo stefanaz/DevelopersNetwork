53faf7f0a110f135e42c5382b8df36f6f3f3a3a5
NStefaniuk <stefan2907@gmail.com>
2020-07-20 11:55:34 +0200

    Delete Profile, User, Posts, Education and Experience

--- src/actions/profile.js
+++ src/actions/profile.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 import { setAlert } from "./alert";
 
-import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from "./types";
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  UPDATE_PROFILE,
+  ACCOUNT_DELETED,
+  CLEAR_PROFILE
+} from "./types";
 
 export const getCurrentProfile = () => async dispatch => {
   try {
@@ -87,7 +93,7 @@ export const addEducation = (formData, history) => async dispatch => {
       }
     };
 
-    console.log(formData);
+    // console.log(formData);
 
     const res = await axios.put("/api/profile/education", formData, config);
     dispatch({
@@ -105,5 +111,59 @@ export const addEducation = (formData, history) => async dispatch => {
       type: PROFILE_ERROR,
       payload: { msg: error.response.statusText, status: error.response.status }
     });
+  }
+};
+
+// delete experience:
+export const deleteExperience = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/profile/experience/${id}`);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+    dispatch(setAlert("Experience Removed", "success"));
+  } catch (error) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
+
+// delete education:
+export const deleteEducation = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/profile/education/${id}`);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+    dispatch(setAlert("Education Removed", "success"));
+  } catch (error) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
+
+/// delete account & profile
+export const deleteAccount = id => async dispatch => {
+  if (window.confirm("Are you sure? ")) {
+    try {
+      const res = await axios.delete("/api/profile");
+      dispatch({ type: CLEAR_PROFILE });
+      dispatch({ type: ACCOUNT_DELETED });
+      dispatch(setAlert("Profile and account Removed", "success"));
+    } catch (error) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: {
+          msg: error.response.statusText,
+          status: error.response.status
+        }
+      });
+    }
   }
 };
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -7,12 +7,13 @@ import DashboardAction from "./DashboardActions";
 import Experience from "./Experience";
 import Education from "./Education";
 import Spinner from "../layout/Spinner";
-import { getCurrentProfile } from "../../actions/profile";
+import { getCurrentProfile, deleteAccount } from "../../actions/profile";
 
 const Dashboard = ({
   getCurrentProfile,
   auth: { user },
-  profile: { profile, loading }
+  profile: { profile, loading },
+  deleteAccount
 }) => {
   useEffect(() => {
     getCurrentProfile();
@@ -30,6 +31,11 @@ const Dashboard = ({
           <DashboardAction />
           <Experience experience={profile.experience} />
           <Education education={profile.education} />
+          <div className='my-2'>
+            <button className='btn btn-danger' onClick={() => deleteAccount()}>
+              <i className='fas fa-user-minus'></i>Delete my Account
+            </button>
+          </div>
         </Fragment>
       ) : (
         <Fragment>
@@ -46,7 +52,8 @@ const Dashboard = ({
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
-  profile: PropTypes.object.isRequired
+  profile: PropTypes.object.isRequired,
+  deleteAccount: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
@@ -56,5 +63,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  { getCurrentProfile }
+  { getCurrentProfile, deleteAccount }
 )(Dashboard);
