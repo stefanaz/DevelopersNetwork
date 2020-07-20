7502b355a41c331905186751d5967f1a560f88e2
NStefaniuk <stefan2907@gmail.com>
2020-07-20 10:49:28 +0200

    Add education & expericence

--- src/actions/profile.js
+++ src/actions/profile.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { setAlert } from "./alert";
 
-import { GET_PROFILE, PROFILE_ERROR } from "./types";
+import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from "./types";
 
 export const getCurrentProfile = () => async dispatch => {
   try {
@@ -27,7 +27,7 @@ export const createProfile = (
   try {
     const config = {
       headers: {
-        "Content-TYpe": "application/json"
+        "Content-Type": "application/json"
       }
     };
     const res = await axios.post("/api/profile", formData, config);
@@ -39,6 +39,63 @@ export const createProfile = (
     if (!edit) {
       history.push("./dashboard");
     }
+  } catch (error) {
+    const errors = error.response.data.errors;
+    if (errors) {
+      errors.forEach(e => dispatch(setAlert(e.msg, "danger")));
+    }
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
+
+// add expuhu:
+export const addExperience = (formData, history) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+    const res = await axios.put("/api/profile/experience", formData, config);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+    dispatch(setAlert("Experience Added", "success"));
+    history.push("./dashboard");
+  } catch (error) {
+    const errors = error.response.data.errors;
+    if (errors) {
+      errors.forEach(e => dispatch(setAlert(e.msg, "danger")));
+    }
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: error.response.statusText, status: error.response.status }
+    });
+  }
+};
+
+// add Edu:
+export const addEducation = (formData, history) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    console.log(formData);
+
+    const res = await axios.put("/api/profile/education", formData, config);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+    dispatch(setAlert("Education Added", "success"));
+    history.push("./dashboard");
   } catch (error) {
     const errors = error.response.data.errors;
     if (errors) {
