97c0bc3d19d49815a08915d1a76cd2e5a57c5363
NStefaniuk <stefan2907@gmail.com>
2020-07-21 10:26:00 +0200

    Posts: reducer, action, component, view

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import AddEducation from "./components/profile-forms/AddEducation";
 import AddExperience from "./components/profile-forms/AddExperience";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
 import PrivateRoute from "./components/routing/PrivateRoute";
 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -66,6 +67,7 @@ const App = () => {
                 path='/add-education'
                 component={AddEducation}
               />
+              <PrivateRoute exact path='/posts' component={Posts} />
             </Switch>
           </section>
         </Fragment>
